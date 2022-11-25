b04d3fbd2b34671e037eb4265ec4ed280dff3617
Sean_Milligan <rseanmilligan@hotmail.com>
2022-11-25 11:12:18 -0800

    mass file transfer

---
+++ majorAirports/static/js/logic.js
@@ -0,0 +1,42 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map("mapid", {
+    center: [
+      30, 30
+    ],
+    zoom: 2
+  });
+
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+
+
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
+
+
+// Accessing the airport GeoJSON URL
+
+let airportData = "https://raw.githubusercontent.com/Scruffy-Bearie/Mapping_Earthquakes/main/majorAirports/static/data/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data, {
+  onEachFeature: function(features, layer) {
+    layer.bindPopup("<h3>" + "Airport Code: " + features.properties.faa + "</h3> <hr> <h3>" + "Airport Name: " + features.properties.name + "</h3>");
+   }
+}).addTo(map);
+});
+
+
